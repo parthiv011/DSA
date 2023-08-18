0900e6a8a6b17cdae43f88d2e61626df4d2add98
parthiv <parmarparthiv77@gmail.com>
2023-08-18 22:34:09 +0530

    implemeneting doubly linked lists

--- LinkedLists/doublyLinkedLists.java
+++ LinkedLists/doublyLinkedLists.java
@@ -14,6 +14,7 @@ public class doublyLinkedLists {
         }
     }
 
+    // insertions in doubly Linked Lists
     private void insertAtHead(int data) {
         if (tail == null) {
             tail = head;
@@ -41,18 +42,62 @@ public class doublyLinkedLists {
             return;
         }
         int cnt = 0;
-        Node newNode = head;
+        Node temp = head;
         while (cnt < position - 1) {
-            newNode = newNode.next;
+            temp = temp.next;
             ++cnt;
         }
-        if (newNode.next == null) {
+        if (temp.next == null) {
             insertAtTail(data);
             return;
         }
+        Node nodeToInsert = new Node(data);
+        nodeToInsert.next = temp.next;
+        temp.next.prev = nodeToInsert;
+        temp.next = nodeToInsert;
+        nodeToInsert.prev = temp;
     }
 
+    // Deletions in Doubly linked lists
     private void deleteNode(int position) {
+        // from head
+        if (position == 0) {
+            if (head != null) {
+                head = head.next;
+                if (head != null) {
+                    head.prev = null;
+                }
+            }
+            System.out.println("Deleted from head!");
+        } else {
+            Node temp = head;
+            int cnt = 0;
+
+            while (temp != null && cnt < position - 1) {
+                temp = temp.next;
+                cnt++;
+            }
+
+            if (temp == null || temp.next == null) {
+                System.out.println("POsition out of range");
+                return;
+            }
+            temp.next = temp.next.next;
+            if (temp.next != null) {
+                temp.next.prev = temp;
+            }
+            System.out.println("Deleted success!");
+        }
+    }
+
+    // size of Linked List
+    private int getLength(Node head) {
+        int length = 0;
+        while (head != null) {
+            length++;
+            head = head.next;
+        }
+        return length;
     }
 
     private void print(Node head) {
@@ -69,7 +114,12 @@ public class doublyLinkedLists {
         dlist.insertAtHead(3);
         dlist.insertAtHead(1);
         dlist.insertAtTail(5);
-        dlist.insertAtPosition(1, 2);
+        dlist.insertAtPosition(3, 2);
         dlist.print(head);
+        dlist.deleteNode(0);
+
+        System.out.println("Size : " + dlist.getLength(head));
+        System.out.println("Head : " + head.data);
+        System.out.println("Tail : " + tail.data);
     }
 }
